ce43396db456aedbe81f403cde53cf2b32847f3b
iamfernanda <fda.isla@gmail.com>
2018-05-25 17:10:52 -0400

    Css: tipografia, color, distribuicion

--- cipher.js
+++ cipher.js
@@ -1,8 +1,22 @@
 window.cipher = {
-  encode: () => {
+  
+  encode: (offset, string) => { /* codificar:() "=>"arrow function   */
     /* Acá va tu código */
+  
+    function encriptar(elemento){
+    var palabra = document.getElementsByTagName("input")[0].value;
+    var palabra_codificada = btoa(palabra);
+    document.getElementById("resultado").innerHTML = palabra_codificada;
+  }
+
+
+  function desencriptar (elemento){
+
+  }  
+  
+  
   },
-  decode: () => {
+  decode: (offset, string) => {
     /* Acá va tu código */
   }
 }
