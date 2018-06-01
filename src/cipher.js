a991cb6f4cf55a8a30cd522d7891a992f57bbb08
iamfernanda <fda.isla@gmail.com>
2018-06-01 15:28:56 -0400

    avance funcion

--- src/cipher.js
+++ src/cipher.js
@@ -1,63 +1,25 @@
-/* Solución facil*/
-// function encriptar(){
-//   var palabra = document.getElementsByTagName("input")[0].value;
-//   var palabra_codificada = btoa(palabra);
-//   document.getElementById("resultado").innerHTML = palabra_codificada;
-// }   
-
-// function desencriptar(){
-//   var palabra = document.getElementsByTagName("input")[0].value;
-//   var palabra_codificada = atob(palabra);
-//   document.getElementById("resultado").innerHTML = palabra_codificada;
-// }   
-
 
 window.cipher = {
 
-  encode: (texto,numero) => {
-    texto = document.getElementById('cipher').value;
-    numero = 33;
-    let cifradas = "";
-    
+   encode : (textReturn, saltos) => {
+    let corriendo ;
+    let resultado = " ";
+    for ( corriendo = 0; i < textReturn.lenhth; corriendo++){
+      let x =textReturn.toUpperCase().charCodeAt(corriendo);
+      let resultadoFormula = ( x - 65 + saltos) % 26 + 65;
+      resultado += String.fromCharCode(resultadoFormula);
+    }
+    return resultado;
+    }
     
   }
 
-  decode: (string,offset) => {
-    
-    
-  }
+   
+
+
+
   
-
 }
 
+  // alert("Estamos en construcción");
 
-
-// encode: (string,offset) => {
-    
-//   string = document.getElementById('cipher').value;
-//   offset = 33;
-//   let cifradas = "";
-  
-//   for (let i = 0; i < string.length; i++) {
-//     const formula = (string.toUpperCase().charCodeAt(i) - 65 + offset) % 26 + 65;
-//     const cifrado = String.fromCharCode(formula);
-//     cifradas+= cifrado;
-    
-//   }
-//   console.log(cifradas);
-//  const contenedor = document.getElementById('containerCC').innerHTML = '<p>'+ cifradas + '</p>';
-// },
-
-// decode: (string,offset) => {
-  
-//   string= document.getElementById('cipher').value;
-//   offset = 33;
-//   let descrifradas = "";
-//   for (let j = 0; j < string.length; j++) {
-//     const formula = (string.toUpperCase().charCodeAt(j) + 65 - offset) % 26 + 65;
-//     const cifrado = String.fromCharCode(formula);
-//     descrifradas+= cifrado;
-// }
-// const contenedor= document.getElementById('containerCC').innerHTML = '<p>' + descrifradas + '</p>';
-// }
-// }
