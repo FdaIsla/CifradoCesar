7fca34816a96fd0593e2a01f4addd844b533c42a
iamfernanda <fda.isla@gmail.com>
2018-06-01 15:28:56 -0400

    avance funcion

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-/* Acá va tu código */
+window.actions = {
+    execute: ()=> {
+    let saltos = document.getElementById("number").value;
+    let textReturn = document.getElementById("cifrado").value;
+      
+    if(saltos == true){
+        document.getElementById("cifrado").value = cipher.encode (textReturn,saltos);
+        
+    } else {
+        alert("usted no a llenado el campo");
+    }
+    
+
+
+}
+}
